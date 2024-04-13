503e5915c16adb77b6e609426380dc6dd67fca0b
Travis Rollins <kalikoze@gmail.com>
2024-04-12 19:53:43 -0700

    DRY up repetitive functions for getting inputs and validating data

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ winning_combinations = {
   "scissors": "paper",
   "paper": "rock",
 }
+
 def random_choice():
   import random
   return random.choice(list(winning_combinations.keys()))
@@ -14,38 +15,22 @@ def get_winner(name, player, computer):
       return f"{name} wins!"
   return "Computer wins!"
 
-def validate_input(player_input):
-  if player_input not in winning_combinations.keys():
-    return False
-  return True
+def invalid_input(input, type="choice"):
+  if type == "name":
+    return input == "" or input.isspace() or input.isdigit()
+  return input not in winning_combinations.keys()
 
-def validate_player(player_name):
-  if player_name == "" or player_name.isspace() or player_name.isdigit():
-    return False
-  return True
-
-def get_player_name():
-  player_name = input("Enter your name: ")
-  if not validate_player(player_name):
-    print("Invalid name. Try again.")
-    return get_player_name()
-  return player_name
-
-def get_player_input():
-  player_input = input("Enter your choice: ")
-  if not validate_input(player_input):
-    print("Invalid choice. Try again.")
-    return get_player_input()
-  return player_input
+def get_input(type):
+  data = input(f"Enter your {type}: ")
+  if invalid_input(data, type):
+    print("Invalid input. Try again.")
+    return get_input(type)
+  return data
 
 def play_game():
   computer_input = random_choice()
-  player_name = get_player_name()
-  player_input = get_player_input()
+  player_name = get_input("name")
+  player_input = get_input("choice")
   return get_winner(player_name, player_input, computer_input)
 
 print(play_game())
-
-# # Path: test_app.py 
-# import pytest
-# from app import get_winner
